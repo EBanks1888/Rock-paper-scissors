bd0e5be51f8b545d2b1c8e4b48a1ffba528c7dce
EthanBanks <e.banks@corecomtechacademy.co.uk>
2024-05-21 14:12:18 +0100

    2nd commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
-package org.example;
+import java.util.Random;
+import java.util.Scanner;
 
-public class Main {
+
+
+public class App {
     public static void main(String[] args) {
+    Random random = new Random();
+    Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello world!");
     }
 }
